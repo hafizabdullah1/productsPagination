9771f929ec0126bf717fce9111e04b3605f8b996
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2023-11-21 02:39:44 +0500

    first

---
+++ src/Components/Products.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Products() {
+
+    const [data, seData] = useState([])
+    const [current, setCurrent] = useState(1)
+    const [productsPerPage, setProductsPerPage] = useState(6)
+    const lastIndex = current * productsPerPage
+    const firstIndex = lastIndex - productsPerPage
+    const currentData = data?.slice(firstIndex, lastIndex)
+    const totalPages = Math.ceil(data?.length / productsPerPage)
+
+    const getAllProduct = async () => {
+        try { await axios.get('https://fakestoreapi.com/products').then((res) => { seData(res.data) }) }
+        catch (error) { console.log(error.message); }
+    }
+    React.useEffect(() => {
+        getAllProduct()
+    }, [])
+
+    return (
+        <div style={{ minHeight: "100vh" }} className='bg-gray-300'>
+            <div className='flex flex-wrap justify-center p-8 gap-8'>
+
+                {currentData.map((ele, index) => (
+                    <div key={index} className="w-80 bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+                        <div className='flex justify-center h-72'>
+                            <img className="p-8 rounded-t-lg" width={280} src={ele.image} alt="product image" />
+                        </div>
+                        <div className="px-5 pb-5">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">{ele.description}</h5>
+                            <div className="flex items-center mt-2.5 mb-5">
+                                <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                </div>
+                                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{ele.rating.rate}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="text-3xl font-bold text-gray-900 dark:text-white">${ele.price}</span>
+                                <button className
+                                    ="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='flex justify-center items-center py-5 gap-1'>
+                {
+                    Array.from({ length: totalPages }).map((_, i) => (
+                        <button key={i} onClick={() => setCurrent(i + 1)} className={i + 1 == current ? 'active border-2 h-10 w-10 border-gray-500' : 'border-2 h-10 w-10 border-gray-500'}>
+                            {i + 1}
+                        </button>
+                    ))
+                }
+            </div>
+        </div>
+
+    )
+}
+
+export default Products
